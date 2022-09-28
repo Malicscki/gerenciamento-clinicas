445ca8eb22fea810f6c355d7caad1fb4370aae65
Malicscki <luiz.malicscki@gmail.com>
2022-09-28 19:27:39 -0300

    teste 19:30

--- src/Devs2Blu.ProjetosAula.OOP3.Main/Cadastros/CadastroPaciente.cs
+++ src/Devs2Blu.ProjetosAula.OOP3.Main/Cadastros/CadastroPaciente.cs
@@ -13,7 +13,7 @@ namespace Devs2Blu.ProjetosAula.OOP3.Main.Cadastros
     {
         public CadastroPaciente()
         {
-            // aula 28/09 
+             //teste 19:30
         }
 
         public void MenuCadastro()
