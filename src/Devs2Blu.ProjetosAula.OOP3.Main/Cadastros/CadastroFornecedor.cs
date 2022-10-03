e721b81c022ad668462722940437a4fc8079b794
Malicscki <luiz.malicscki@gmail.com>
2022-10-03 18:02:20 -0300

    cadastro fornecedor

--- src/Devs2Blu.ProjetosAula.OOP3.Main/Cadastros/CadastroFornecedor.cs
+++ src/Devs2Blu.ProjetosAula.OOP3.Main/Cadastros/CadastroFornecedor.cs
@@ -3,44 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Devs2Blu.ProjetosAula.OOP3.Main.Interfaces;
 using Devs2Blu.ProjetosAula.OOP3.Main.Utils;
 using Devs2Blu.ProjetosAula.OOP3.Main.Utils.Enums;
 using Devs2Blu.ProjetosAula.OOP3.Models.Model;
 
 namespace Devs2Blu.ProjetosAula.OOP3.Main.Cadastros
 {
-    public class CadastroFornecedor
+    public class CadastroFornecedor : IMenuCadastro
     {
-        public CadastroFornecedor()
-        {
-
-        }
-
-        public void MenuCadastro()
+        public Int32 MenuCadastro()
         {
             Int32 opcao;
 
-            do
-            {
+            Console.Clear();
+            Console.WriteLine("----- Cadastro de Fornecedores --------");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("----- 1- Lista de Fornecedores --------");
+            Console.WriteLine("----- 2- Cadastro de Fornecedores -----");
+            Console.WriteLine("----- 3- Alterar Fornecedores ---------");
+            Console.WriteLine("----- 4- Excluir Fornecedores ---------");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("----- 0- Sair -------------------------");
+            Int32.TryParse(Console.ReadLine(), out opcao);
+            return opcao;
 
-                Console.WriteLine("----- Cadastro de Fornecedores -----");
-                Console.WriteLine("----- 1- Lista de Fornecedores -----");
-                Console.WriteLine("----- 2- Cadastro de Fornecedor -----");
-                Console.WriteLine("----- 3- Alterar Fornecedor -----");
-                Console.WriteLine("---------------------");
-                Console.WriteLine("----- 0- Sair -----");
-                Int32.TryParse(Console.ReadLine(), out opcao);
+        }
 
-                switch (opcao)
-                {
-                    case (int)MenuEnums.LISTAR:
-                        ListarFornecedores();
-                        break;
-                    default:
-                        break;
-                }
-
-            } while (!opcao.Equals(MenuEnums.SAIR));
+        public void Listar()
+        {
+            ListarFornecedores();
         }
 
         public void ListarFornecedores()
@@ -49,29 +41,152 @@ namespace Devs2Blu.ProjetosAula.OOP3.Main.Cadastros
 
             foreach (Fornecedor fornecedor in Program.Mock.ListaFornecedores)
             {
-                Console.WriteLine("\t-----------------------------------------");
-                Console.WriteLine($"\tFornecedor: {fornecedor.CodigoFornecedor}");
-                Console.WriteLine($"\tNome: {fornecedor.Nome}");
-                Console.WriteLine($"\tTipo: {fornecedor.TipoFornecedor}");
-                Console.WriteLine($"\tCNPJ: {fornecedor.CGCCPF}");
-                Console.WriteLine("\t-----------------------------------------\n");
+                Console.WriteLine("-------------------------------------------------");
+                Console.WriteLine($"Código Fornecedor: {fornecedor.CodigoFornecedor}");
+                Console.WriteLine($"Nome: {fornecedor.Nome}");
+                Console.WriteLine($"Tipo Fornecedor: {fornecedor.TipoFornecedor}");
+                Console.WriteLine($"CPF: {fornecedor.CGCCPF}");
+                Console.WriteLine("-----------------------------------------------\n");
             }
+            Console.ReadLine();
         }
+
 
         public void CadastrarFornecedor(Fornecedor novoFornecedor)
         {
             Program.Mock.ListaFornecedores.Add(novoFornecedor);
         }
 
-        public void AlterarFornecedor()
+        public void Cadastrar()
         {
+
+
+            Console.Clear();
+
+            
+            Fornecedor novoFornecedor = new Fornecedor(); 
+            Random rd = new Random();
+
+
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine($"Cadastro de Fornecedores");
+            Console.WriteLine("-----------------------------------------");
+
+            novoFornecedor.Codigo = rd.Next(1, 300);
+            Console.WriteLine($"Código: {novoFornecedor.Codigo}");
+
+            Console.WriteLine($"Insira o nome do fornecedor");
+            novoFornecedor.Nome = Console.ReadLine();
+
+            Console.WriteLine($"Insira o CPF do fornecedor");
+            novoFornecedor.CGCCPF = Console.ReadLine();
+
+            Console.WriteLine($"Insira o tipo de fornecedor");
+            novoFornecedor.TipoFornecedor = Console.ReadLine();
+
+
+            novoFornecedor.CodigoFornecedor = rd.Next(1, 300);
+            Console.WriteLine("Fornecedor Cadastrado com Sucesso!\n");
+
+
+            CadastrarFornecedor(novoFornecedor);
+            Console.ReadKey();
 
         }
 
-        public void ExcluirFornecedor()
+        private void ListarFornecedoresByCodeAndName()
+        {
+            foreach (Fornecedor fornecedor in Program.Mock.ListaFornecedores)
+            {
+                Console.Write($"| {fornecedor.CodigoFornecedor} - {fornecedor.Nome} ");
+            }
+            Console.WriteLine("\n");
+        }
+
+
+
+        public void AlterarFornecedor(Fornecedor fornecedor)
         {
 
+            var frn = Program.Mock.ListaFornecedores.Find(f => f.CodigoFornecedor == fornecedor.CodigoFornecedor);
+            int index = Program.Mock.ListaFornecedores.IndexOf(frn);
+            Program.Mock.ListaFornecedores[index] = fornecedor;
+
+        }
+        public void Alterar()
+        {
+            Console.Clear();
+            Fornecedor fornecedor;
+            int codigoFornecedor;
+
+            Console.WriteLine("Informe o Recepcionista que Deseja Alterar:\n");
+            ListarFornecedoresByCodeAndName();
+
+            Int32.TryParse(Console.ReadLine(), out codigoFornecedor);
+
+            fornecedor = Program.Mock.ListaFornecedores.Find(f => f.CodigoFornecedor == codigoFornecedor);
+
+            const int NOME = 1, CPF = 2, TIPOFORNECEDOR = 3;
+            int opcaoAlterar;
+            bool alterar = true;
+
+            do
+            {
+                Console.WriteLine($"Código do Fornecedor: {fornecedor.Codigo}/{fornecedor.CodigoFornecedor} | Nome: {fornecedor.Nome} | CPF: {fornecedor.CGCCPF} | Tipo Fornecedor: {fornecedor.TipoFornecedor}");
+                Console.WriteLine("Qual campo deseja alterar?");
+                Console.WriteLine("1 - Nome | 2 - CPF | 3 - Tipo Fornecedor | 0 - SAIR"); 
+                Int32.TryParse(Console.ReadLine(), out opcaoAlterar);
+
+                switch (opcaoAlterar)
+                {
+                    case NOME:
+                        Console.WriteLine("Informe um novo nome:");
+                        fornecedor.Nome = Console.ReadLine();
+                        break;
+                    case CPF:
+                        Console.WriteLine("Informe um novo CPF:");
+                        fornecedor.CGCCPF = Console.ReadLine();
+                        break;
+                    case TIPOFORNECEDOR:
+                        Console.WriteLine("Informe o novo Tipo de Fornecedor:");
+                        fornecedor.TipoFornecedor = Console.ReadLine();
+                        break;
+
+                    default:
+                        alterar = false;
+                        break;
+                }
+
+                if (alterar)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Dado Alterado com Sucesso!");
+                }
+            } while (alterar);
+
+            AlterarFornecedor(fornecedor);
+        }
+
+        private void ExcluirFornecedor(Fornecedor fornecedor)
+        {
+            Program.Mock.ListaFornecedores.Remove(fornecedor);
+        }
+
+        public void Excluir()
+        {
+            Console.Clear();
+            Fornecedor fornecedor = new Fornecedor();
+            int codFornecedor;
+
+            Console.WriteLine("Informe o Recepcionista que Deseja Excluir:\n");
+            ListarFornecedoresByCodeAndName();
+            Int32.TryParse(Console.ReadLine(), out codFornecedor);
+            fornecedor = Program.Mock.ListaFornecedores.Find(f => f.CodigoFornecedor == codFornecedor);// Função Lambda
+            Console.WriteLine($"Fornecedor Excluido: {fornecedor.CodigoFornecedor}\n");
+            Console.ReadKey();
+
+
+            ExcluirFornecedor(fornecedor);
         }
     }
 }
-
